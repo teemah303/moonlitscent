66f0bc1390be6fe0a376fccd494cc5633a494531
teemah303 <amir.m1700769@st.futminna.edu.ng>
2026-06-11 16:14:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a').forEach(link=>{
+
+link.addEventListener('click',function(e){
+
+if(this.hash !== ''){
+
+e.preventDefault();
+
+document.querySelector(this.hash)
+.scrollIntoView({
+behavior:'smooth'
+});
+
+}
+
+});
+
+});
