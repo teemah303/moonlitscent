c9bbeffdda12aa09c54dea3ffbeff95ea297b305
teemah303 <amir.m1700769@st.futminna.edu.ng>
2026-06-12 21:21:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,9 @@ behavior:'smooth'
 });
 
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
